773142ba3fa5b727567eb1ce626870a44d287af8
wangqingzhu <wangqingzhu@lanjingren.com>
2019-05-14 20:26:56 +0800

    add eslint rules

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ module.exports = {
   plugins: ['react'],
   extends: ['manpacker'],
   rules: {
-    'react/jsx-uses-vars': 'error'
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-curly-spacing': ['error', { 'when': 'always', 'children': true }],
+    'react/prefer-es6-class': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/self-closing-comp': 'warn',
+    'no-extra-boolean-cast': 'warn',
+    'react/no-deprecated': 'warn',
+    'no-unreachable': 'warn',
+    'no-mixed-spaces-and-tabs': 'warn',
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/jsx-key': 'error',
+    'react/jsx-max-props-per-line': ['warn', { 'maximum': 1 }],
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-sort-props': 'error',
+    'react/jsx-uses-react': 'warn',
+    'react/no-unknown-property': 'error',
+    'react/jsx-closing-bracket-location': 'error'
   }
 }
